a406aed28bfcf67363db45906fb34deca6b2d410
Lydia Wildes <lydiawildes1@gmail.com>
2022-11-02 16:08:44 -0500

    set up pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,7 +1,18 @@
 const pg = require('pg');
 
-let pool = new pg.Pool({
-  database: "cloud-todo"
-});
+
+
+let pool;
+
+if (process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL
+  })
+}
+
+else {pool = new pg.Pool({
+  database: "cloud-todo" ,
+})
+};
 
 module.exports = pool;
